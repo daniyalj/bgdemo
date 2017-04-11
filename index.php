b0b1e88ff97e1df8e5c194092dd741e74c5095aa
daniyalj <daniyaljavedk@gmail.com>
2017-04-10 22:35:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
         <div class="col-md-6 col-md-offset-3">
           <h2>Application Example</h2>
           <p>
-            <img src="one.jpeg">
+            <img src="https://cloud.githubusercontent.com/assets/13129393/24668479/07afa97c-1935-11e7-9b6f-9f9964117979.png" alt="ms">
           </p>
         </div>
       </div>
